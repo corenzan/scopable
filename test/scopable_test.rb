48a4a75ee61bd4f4c6894e944eb564d2725d3b24
Arthur <arthur@corenzan.com>
2017-06-14 14:20:55 -0300

    Update tests again using Model mock class

--- test/scopable_test.rb
+++ test/scopable_test.rb
@@ -2,6 +2,34 @@ require 'minitest/autorun'
 require 'active_support/testing/declarative'
 
 require_relative '../lib/scopable'
+require_relative 'support/model'
+
+class TestModel < Minitest::Test
+  extend ActiveSupport::Testing::Declarative
+
+  test '#initialize' do
+    model = Model.new
+    assert_instance_of(Model, model)
+  end
+
+  test '#scopes' do
+    model = Model.new
+    assert_respond_to(model, :scopes)
+  end
+
+  test 'scope' do
+    model = Model.new(:scope)
+    assert_instance_of(Model, model.scope)
+    assert_equal(true, model.scopes[:scope])
+  end
+
+  test 'multiple scopes' do
+    model = Model.new(:a, :b, :c)
+    assert_respond_to(model, :a)
+    assert_respond_to(model, :b)
+    assert_respond_to(model, :c)
+  end
+end
 
 class TestScopable < Minitest::Test
   extend ActiveSupport::Testing::Declarative
@@ -23,6 +51,8 @@ class TestScopable < Minitest::Test
   test '#apply with empty params passes through' do
     vehicle_scope = Class.new(Scopable) do
       model :car
+      scope :year
+      scope :model
     end
     assert_equal(:car, vehicle_scope.new.apply)
   end
@@ -34,60 +64,85 @@ class TestScopable < Minitest::Test
     assert_equal(:bike, vehicle_scope.apply)
   end
 
-  test 'no matching scope passes through' do
-    pet = OpenStruct.new(mammal: nil)
+  test 'without matching scope passes through' do
+    pet = Model.new(:mammal)
     pet_scope = Class.new(Scopable) do 
       model pet
-      scope :mammal=
+      scope :mammal
     end
-    assert_nil(pet_scope.apply.mammal)
+    assert_nil(pet_scope.apply.scopes[:mammal])
   end
 
   test 'no options' do
-    person = OpenStruct.new(profile: nil)
+    person = Model.new(:profile)
     person_scope = Class.new(Scopable) do 
       model person
-      scope :profile=
+      scope :profile
     end
-    person_scope.apply('profile=' => 'fat')
-    assert_equal('fat', person.profile)
+    assert_equal('fat', person_scope.apply(profile: 'fat').scopes[:profile])
   end
 
   test 'truthy values' do
-    user = OpenStruct.new(active: nil)
+    user = Model.new(:active)
     user_scope = Class.new(Scopable) do 
       model user
-      scope :active=
+      scope :active
     end
-    user_scope.apply('active=' => 'true')
-    assert_equal(true, user.active)
-    user_scope.apply('active=' => 'yes')
-    assert_equal(true, user.active)
-    user_scope.apply('active=' => 'on')
-    assert_equal(true, user.active)
+    assert_equal(true, user_scope.apply(active: 'true').scopes[:active])
+    assert_equal(true, user_scope.apply(active: 'yes').scopes[:active])
+    assert_equal(true, user_scope.apply(active: 'on').scopes[:active])
   end
 
   test 'falsy values' do
-    user = OpenStruct.new(active: nil)
+    user = Model.new(:active)
     user_scope = Class.new(Scopable) do 
       model user
-      scope :active=
+      scope :active
     end
-    user_scope.apply('active=' => 'false')
-    assert_equal(false, user.active)
-    user_scope.apply('active=' => 'no')
-    assert_equal(false, user.active)
-    user_scope.apply('active=' => 'off')
-    assert_equal(false, user.active)
+    assert_equal(false, user_scope.apply(active: 'false').scopes[:active])
+    assert_equal(false, user_scope.apply(active: 'no').scopes[:active])
+    assert_equal(false, user_scope.apply(active: 'off').scopes[:active])
   end
 
   test 'option :param' do
-    book = OpenStruct.new(query: nil)
+    book = Model.new(:query)
     book_scope = Class.new(Scopable) do 
       model book
-      scope :query=, param: :q
+      scope :query, param: :q
     end
-    book_scope.apply(q: 'mermaids')
-    assert_equal('mermaids', book.query)
+    assert_equal('mermaids', book_scope.apply(q: 'mermaids').scopes[:query])
+  end
+
+  test 'option :value' do
+    file = Model.new(:name)
+    file_scope = Class.new(Scopable) do 
+      model file
+      scope :name, value: 'a file'
+    end
+    assert_equal('a file', file_scope.apply(name: 'explorer.exe').scopes[:name])
+  end
+
+  test 'option :default' do
+    directory = Model.new(:name)
+    directory_scope = Class.new(Scopable) do 
+      model directory
+      scope :name, default: 'system32'
+    end
+    assert_equal('system32', directory_scope.apply.scopes[:name])
+    assert_equal('windows', directory_scope.apply(name: 'windows').scopes[:name])
+  end
+
+  test 'option :required' do
+    account = Model.new(:number) do
+      def none
+        :none
+      end
+    end
+    account_scope = Class.new(Scopable) do 
+      model account
+      scope :number, required: true
+    end
+    assert_equal(:none, account_scope.apply)
+    assert_equal('123', account_scope.apply(number: '123').scopes[:number])
   end
 end
--- test/support/model.rb
+++ test/support/model.rb
@@ -1,11 +1,16 @@
 class Model
-  def self.scopes
+  def scopes
     @scopes ||= {}
   end
 
-  def self.scope(name)
-    define_singleton_method(name) do |value = true|
-      scopes.store(name, value)
+  def initialize(*attrs, &block)
+    attrs.each do |name|
+      define_singleton_method(name) do |value = true|
+        scopes.store(name, value)
+        self
+      end
     end
+
+    instance_exec(&block) if block_given?
   end
 end
